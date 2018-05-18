870e565b4af30bba70d03cf9bb9eb2b58c08324b
shatyaki_dutt@hotmail.com <shatyaki_dutt@hotmail.com>
2018-05-18 17:31:36 +0530

    Hashmap added

--- PlayerMatcher/src/main/java/com/stackroute/maverick/controller/PlayerMatcherController.java
+++ PlayerMatcher/src/main/java/com/stackroute/maverick/controller/PlayerMatcherController.java
@@ -1,22 +1,38 @@
 package com.stackroute.maverick.controller;
 
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Controller;
 
+import com.stackroute.maverick.domain.User;
+
 
 @Controller
 public class PlayerMatcherController {
-
 	
+	@Bean
+	public User user() {
+		return new User();
+	}
 	
+	@Autowired
+	User user;
+    
 	
-	
-	
-	@KafkaListener(topics = "myTopic.t")
 	public ResponseEntity<String> addPlayer(){
-		
+		Set<String> set = new HashSet<String>();
+		Map<Integer,Set<String>> map = new HashMap<Integer,Set<String>>();
+		set.add("ABC");
+	    set.add("DEF");
+	    map.put(123,set);
 		return null;
 	}
 }
--- PlayerMatcher/src/main/java/com/stackroute/maverick/domain/User.java
+++ PlayerMatcher/src/main/java/com/stackroute/maverick/domain/User.java
@@ -1,10 +1,12 @@
 package com.stackroute.maverick.domain;
 
-import javax.persistence.Entity;
 
-@Entity
+
+
 public class User {
 	
-
+public User()
+{
+	}
 }
 	
